dd76d3cb805afbce97fc731557845f6e68aab7e0
Mario Enrique <mebrito_96@hotmail.com>
2020-10-22 15:25:55 -0500

    form_2 added

--- DataBase_Formulary/Form1.cs
+++ DataBase_Formulary/Form1.cs
@@ -1,4 +1,10 @@
-﻿using MySql.Data.MySqlClient;
+﻿using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -26,11 +32,6 @@ namespace DataBase_Formulary
 
         private void button1_Click(object sender, EventArgs e)
         { 
-            //String fullName = txtNombre.Text;
-            //int edad = int.Parse(txtEdad.Text);
-            //String sex = txtSex.Text;
-            //String address = txtAddress.Text;
-            //String description = txtDescription.Text;
 
             String insertChain = "INSERT INTO pacientes_2 (nombre, edad, sexo, direccion, descripcion) VALUES ('"+ txtNombre.Text +
                 "', '" + int.Parse(txtEdad.Text) + "','" + txtSex.Text + "','" + txtAddress.Text + "', '" + txtDescription.Text + "')";
@@ -40,6 +41,37 @@ namespace DataBase_Formulary
 
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //I use the library itext 7 from NuGet
+            String nombre = "Nombre: " + txtNombre.Text + "\n";
+            String edad = "Edad: " + txtEdad.Text + "\n";
+            String sex = "Sexo: " + txtSex.Text + "\n";
+            String adds = "Dirección: " + txtAddress.Text + "\n";
+            String description = "Causa de cita: " + txtDescription.Text + "\n";
+            PDFCreator(nombre, edad, sex, adds, description);
+        }
+        private void PDFCreator(String name, String age, String sex, String adds, String description)
+        {
+
+            //Initialize PDF writer and set the file address
+            PdfWriter pdfwriter = new PdfWriter("C:/Users/mebri/Documents/GitHub/C#_Formulary_database_connection/C-_" +
+                                  "Formulary_database_connection/DataBase_Formulary/Reportes/Reporte.pdf");
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(pdfwriter); 
+            Document documento = new Document(pdf, PageSize.LETTER); //I give size to the document
+            documento.SetMargins(60, 20, 55, 20);//I put a margin to the document (top, rigth, button, left)
+            //Document Fonts
+            PdfFont fontTitle = PdfFontFactory.CreateFont(StandardFonts.TIMES_BOLD);
+            PdfFont fontContend = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
+            //Document Sections
+            var Title = new Paragraph("Historia Clinica de: " + txtNombre.Text + "\n").SetFont(fontTitle);
+            var pxInformation = new Paragraph(name + age + sex + adds + description).SetFont(fontContend); 
+            //Add Sections
+            documento.Add(Title);
+            documento.Add(pxInformation);
+            documento.Close();
+        }
         public void  ereaserFiels()
         {
             txtNombre.Text = "";
@@ -49,5 +81,6 @@ namespace DataBase_Formulary
             txtDescription.Text = "";
         }
 
+        
     }
 }
